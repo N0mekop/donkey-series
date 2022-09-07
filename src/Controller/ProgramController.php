485ac3da39a96dd1a9922c25b32c52243004010f
N0mekop <blinkevin@hotmail.com>
2022-09-07 14:22:17 +0200

    Route dans ProgramController vers show.html.twig

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,11 @@ class ProgramController extends AbstractController
             'website' => 'Donkey Séries',
         ]);
     }
+    #[Route('/program/{id<\d+>}', methods: ["GET"], name: 'program_id')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
 }
