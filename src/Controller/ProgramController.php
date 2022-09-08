8b330132a32a4894e6df6dd727e0d3b2ea64794f
N0mekop <blinkevin@hotmail.com>
2022-09-08 15:48:05 +0200

    Dossier category + twigs dans \templates, modif twgis dans \program, modifs Controllers

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,24 +2,46 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/program", name: "program_")]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ProgramRepository $ProgramRepository): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Donkey Séries',
-        ]);
+        $programs = $ProgramRepository
+            ->findAll();
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+        return $this->render(
+            'program/index.html.twig',
+            ['website' => 'Donkey Séries', 'programs' => $programs]
+        );
     }
-    #[Route('/program/{id<\d+>}', methods: ["GET"], name: 'program_id')]
-    public function show(int $id): Response
+    #[Route("/{id<^[0-9]+$>}", name: "show")]
+    public function show(int $id, ProgramRepository $ProgramRepository): Response
     {
-        return $this->render('program/show.html.twig', [
-            'id' => $id,
-        ]);
+        $program = $ProgramRepository
+            ->findOneBy(
+                ['id' => $id]
+            );
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+        return $this->render(
+            'program/show.html.twig',
+            [
+                'program' => $program,
+            ]
+        );
     }
 }
